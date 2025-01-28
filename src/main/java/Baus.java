c12f193e2a0098ca8f0bc81a46c849aa88f921f2
Chen Yiyang <141634187+yiyang74@users.noreply.github.com>
2025-01-28 23:53:57 +0800

    Add ability to store and display stored text

--- src/main/java/Baus.java
+++ src/main/java/Baus.java
@@ -1,8 +1,18 @@
 import java.util.Scanner;
 
 public class Baus {
+
+    private static final String lineSeparator = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+
+    private static void printList(String[] items, int itemsLength) {
+        System.out.print(lineSeparator);
+        for (int i = 0; i < itemsLength; i++) {
+            System.out.println((i + 1) + ". " + items[i]);
+        }
+        System.out.print(lineSeparator);
+    }
+
     public static void main(String[] args) {
-        String lineSeparator = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
         System.out.print(
                 lineSeparator +
                 "Hello! I'm Baus\n" +
@@ -11,10 +21,17 @@ public class Baus {
                 "Bye. Hope to see you again soon!\n" +
                 lineSeparator
         );
+        String[] tasks = new String[100];
         Scanner in = new Scanner(System.in);
         String task = in.nextLine();
+        int i = 0;
         while (!task.equals("bye")) {
-            System.out.print(lineSeparator + task + '\n' + lineSeparator);
+            if (task.equals("list")) {
+                printList(tasks, i);
+            } else {
+                tasks[i++] = task;
+                System.out.print(lineSeparator + "added: " + task + '\n' + lineSeparator);
+            }
             task = in.nextLine();
         }
         System.out.print(lineSeparator + "Bye... :(\nHope to see you again soon! >///<\n" + lineSeparator);
